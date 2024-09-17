5d1c35b9614276dda9d9ab5f279f8c8121865090
geekz <jjohan91@hotmail.com>
2024-09-17 16:30:47 +0200

    Abstract UseError class and its subclasses

---
+++ OOPBasics/NullInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasics
+{
+    internal class NullInputError: UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to submit an empty or null input. This fired an error!";
+        }
+    }
+}
--- OOPBasics/Program.cs
+++ OOPBasics/Program.cs
@@ -10,9 +10,19 @@
                 //Person person = new Person();
                 //person.Age = 10;
                 //person.Weight = -1;
-                PersonHandler handler = new PersonHandler();
-                handler.CreatePerson(33, "Johan", "Ahlsson", 185, 75);
-                handler.CreatePerson(23, "", "hejfdsalpfdsafpdsaåfdlspåa", -1, 0);
+                //PersonHandler handler = new PersonHandler();
+                //handler.CreatePerson(33, "Johan", "Ahlsson", 185, 75);
+                //handler.CreatePerson(23, "", "hejfdsalpfdsafpdsaåfdlspåa", -1, 0);
+
+                List<UserError> errors = new List<UserError>();
+                errors.Add(new NumericInputError());
+                errors.Add(new TextInputError());
+                errors.Add(new NullInputError());
+
+                foreach (UserError error in errors)
+                {
+                    Console.WriteLine(error.UEMessage());
+                }
             }
             catch (Exception ex)
             {
